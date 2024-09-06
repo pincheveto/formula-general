3d7c78bffe19ff90f1890f8933d68c9f33d24713
Carlos Ortiz (Veto) <168112824+pincheveto@users.noreply.github.com>
2024-09-06 16:41:52 -0600

    Add files via upload

---
+++ general-formula/gFormula.js
@@ -0,0 +1,14 @@
+let x1 = 0;
+let x2 = 0;
+let a = 2;
+let b = 9;
+let c = 10;
+
+let resRaiz = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+console.log(resRaiz);
+
+x1 = (-b + resRaiz) / (2 * a);
+console.log(x1);
+
+x2 = (-b - resRaiz) / (2 * a);
+console.log(x2);
